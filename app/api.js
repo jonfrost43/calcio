78562eab911e01e71ceff1a1a79bfe7966a24c05
Jon Frost <jonfrost43@gmail.com>
2015-10-19 17:55:08 +0100

    table service rendering to template

--- app/api.js
+++ app/api.js
@@ -2,28 +2,73 @@ var _ = require('lodash'),
 	fs = require('fs');
 
 var tablify = function(tables, match, index, array){
-	var teamBaseValues = {
-		"played": 0,
-		"won": 0,
-		"drawn": 0,
-		"lost": 0,
-		"gd": 0,
-		"points": 0,
+	var group = match.group,
+		homeTeam = match.home.name,
+		awayTeam = match.away.name,
+		homeTeamGoals = match.home.goals ? match.home.goals.length : 0,
+		awayTeamGoals = match.away.goals ? match.away.goals.length : 0;
+
+	var getBaseValues = function(teamName){
+		return {
+			name: teamName,
+			played: 0,
+			won: 0,
+			drawn: 0,
+			lost: 0,
+			gd: 0,
+			points: 0
+		};
 	};
 
-	if(!tables[match.group]){
-		tables[match.group] = {};
+	//create group if new
+	if(!tables[group]){
+		tables[group] = [];
 	}
 
-	if(!tables[match.group][match.home.name]){
-		tables[match.group][match.home.name] = teamBaseValues;
+	var homeData = _.findWhere(tables[group], {name: homeTeam}),
+		awayData = _.findWhere(tables[group], {name: awayTeam});
+
+	//create team if home team is new
+	if(!homeData){
+	//if(!tables[group][homeTeam]){
+		homeData = getBaseValues(homeTeam);
+		tables[group].push(homeData);
 	}
 
-	if(!tables[match.group][match.away.name]){
-		tables[match.group][match.away.name] = teamBaseValues;
+	//create team if away team is new
+	if(!awayData){
+	//if(!tables[group][awayTeam]){
+		awayData = getBaseValues(awayTeam);
+		tables[group].push(awayData);
 	}
+
+	homeData.played++;
+	awayData.played++;
+
+	if(homeTeamGoals > awayTeamGoals) {
+		homeData.won++;
+		awayData.lost++;
+	}
+	else if(homeTeamGoals === awayTeamGoals) {
+		homeData.drawn++;
+		awayData.drawn++;
+	}
+	else if(homeTeamGoals < awayTeamGoals) {
+		homeData.lost++;
+		awayData.won++;
+	}
+
+	homeData.gd = homeData.gd + (homeTeamGoals - awayTeamGoals);
+	awayData.gd = awayData.gd + (awayTeamGoals - homeTeamGoals);
+
+	homeData.points = (homeData.won * 3) + homeData.drawn;
+	awayData.points = (awayData.won * 3) + awayData.drawn;
 
 	return tables;
+};
+
+var applyResult = function(teamData, match){
+
 };
 
 exports.handler = function(request, response){
@@ -41,7 +86,7 @@ exports.handler = function(request, response){
 		}
 
 		if(request.params.format === 'tables'){
-			data = data.reduce(tablify, {})
+			data = data.reduce(tablify, {});
 		}
 
 		response.send(data);
