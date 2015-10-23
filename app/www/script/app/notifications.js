8fa7f8b154a99ec4b2c3c273a0cec13d8ed23f5c
Jon Frost <jonfrost43@gmail.com>
2015-10-23 14:12:29 +0100

    styling and animation for notifications

--- app/www/script/app/notifications.js
+++ app/www/script/app/notifications.js
@@ -9,19 +9,31 @@ function($, component){
 	var notification = component({
 		initialise: function($el, opts){
 			if(!$el || !$el.length){
-				$el = $('<p>');
+				$el = $('<div class="notification"><p></div>');
 			}
+
+			var $p = $el.find('p');
 
 			if(!opts.text){
 				opts.text = 'Blank message'
 			}
 
-			$el.text(opts.text);
+			$el.on('transitionend', function(){
+				if($el.hasClass('hide')){
+					$el.remove();
+				}
+			});
+
+			$p.text(opts.text);
 
 			$container.append($el);
 
+			//wait for a repaint, otherwise the CSS3 transition doesn't run
+			$el[0].clientHeight;
+			$el.addClass('show');
+
 			setTimeout(function(){
-				$el.remove()
+				$el.addClass('hide');
 			}, 3000);
 		}
 	});
